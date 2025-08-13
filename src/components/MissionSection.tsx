3ea951ebe69579c9c11e20baf436bbb451043a3b
avantikablx <avantika.blx@gmail.com>
2025-08-13 10:12:12 +0530

    Reduce mission section height by 10%

--- src/components/MissionSection.tsx
+++ src/components/MissionSection.tsx
@@ -1,6 +1,6 @@
 const MissionSection = () => {
     return (
-        <section className="relative bg-black text-white min-h-[500px] sm:min-h-[600px] lg:min-h-[700px] py-16 sm:py-20 lg:py-28 overflow-hidden w-screen"
+        <section className="relative bg-black text-white min-h-[450px] sm:min-h-[540px] lg:min-h-[630px] py-14 sm:py-18 lg:py-25 overflow-hidden w-screen"
             style={{
                 backgroundImage: "url('https://cosmosmagazine.com/wp-content/uploads/2022/10/sellmore.Getty_.jpg')",
                 backgroundSize: "cover",
@@ -11,7 +11,7 @@ const MissionSection = () => {
             <div className="absolute inset-0 bg-black/60 z-0"></div>
 
             {/* Content */}
-            <div className="relative z-10 w-full px-4 sm:px-6 lg:px-8 flex flex-col gap-6 justify-center min-h-[300px] sm:min-h-[400px] lg:min-h-[500px]">
+            <div className="relative z-10 w-full px-4 sm:px-6 lg:px-8 flex flex-col gap-6 justify-center min-h-[270px] sm:min-h-[360px] lg:min-h-[450px]">
                 {/* Heading */}
                 <div className="text-center">
                     <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-3xl xl:text-4xl font-bold drop-shadow-lg" style={{ textShadow: '1px 1px 2px rgb(0, 0, 0)' }}>Our Mission</h2>
